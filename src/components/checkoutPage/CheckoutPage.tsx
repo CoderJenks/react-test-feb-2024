8a3dcc0c484c5754aeb5e8caea1f4d406c0f2468
Sam <89224927+CoderJenks@users.noreply.github.com>
2024-02-29 13:48:07 +0000

    add styling to checkout page

--- src/components/checkoutPage/CheckoutPage.tsx
+++ src/components/checkoutPage/CheckoutPage.tsx
@@ -1,17 +1,20 @@
 import { useContext } from "react"
 import { BasketItemType, ShoppingBasketContext } from "../../context/ShoppingBasketContext"
+import "./CheckoutPage.scss";
 
 const CheckoutPage = () => {
     const { basketItems } = useContext(ShoppingBasketContext);
 
     function BasketProductCard({ id, colour, name, price, img, quantity }:BasketItemType) {
         return (
-            <div className="BasketProductCard" _data-testid={`ProductCard-${id}`}>
+            <div className="BasketProductCard__Container" _data-testid={`ProductCard-${id}`}>
                 <img className="BasketProductCard__Image" src={img} alt={name} />
-                <h3 className="BasketProductCard__Name">{name}</h3>
-                <h4 className="BasketProductCard__Colour">Colour: {colour}</h4>
-                <div className="BasketProductCard__Price">Price: {new Intl.NumberFormat("en-GB", { style: "currency", currency: "GBP" }).format(price)}</div>
-                <div className="BasketProductCard__Quantity">Quantity in basket: {quantity}</div>
+                <div className="BasketProductCard__ProductDetails" >
+                    <h3 className="BasketProductCard__Name">{name}</h3>
+                    <h4 className="BasketProductCard__Colour">Colour: {colour}</h4>
+                    <div className="BasketProductCard__Price">Price: {new Intl.NumberFormat("en-GB", { style: "currency", currency: "GBP" }).format(price)}</div>
+                    <div className="BasketProductCard__Quantity">Quantity in basket: {quantity}</div>
+                </div>
             </div>
         )
     }
@@ -42,7 +45,7 @@ const CheckoutPage = () => {
 
     return (
         <>
-            <div className="CheckoutPage">
+            <div className="CheckoutPage__Content">
                 <h2 className="CheckoutPage__Header">
                     Checkout
                 </h2>
