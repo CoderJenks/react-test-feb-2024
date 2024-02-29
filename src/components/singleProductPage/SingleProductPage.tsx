b5188649352fed1f1107e7bf880a21713e5afac2
Sam <89224927+CoderJenks@users.noreply.github.com>
2024-02-29 13:42:34 +0000

    add styling to single product page

--- src/components/singleProductPage/SingleProductPage.tsx
+++ src/components/singleProductPage/SingleProductPage.tsx
@@ -3,6 +3,7 @@ import { ProductResponse, getProduct } from "../../api/productsApi"
 import { useContext, useEffect, useState } from "react";
 import useProductId from "../../hooks/useProductId";
 import { ShoppingBasketContext } from "../../context/ShoppingBasketContext";
+import "./SingleProductPage.scss";
 
 const SingleProductPage = () => {    
     const [ product, setProduct ] = useState<ProductResponse>();
@@ -24,24 +25,25 @@ const SingleProductPage = () => {
     
     return (
         <div className="SingleProductPage__Content">
-            <h2 className="MainPage__Header">
+            <h2 className="SingleProductPage__Header">
                 Single Product Page
             </h2>
             {(product) ? (
-                <>
-                    <h3 className="ProductCard__Name">{product.name}</h3>
-                    <img className="ProductCard__Image" src={product.img} alt={product.name} />
-                    <h4 className="ProductCard__Colour">Colour: {product.colour}</h4>
-                    <div className="ProductCard__Price">Price: {new Intl.NumberFormat("en-GB", { style: "currency", currency: "GBP" }).format(product.price)}</div>
-                    <div className="ProductCard__ID">Product ID: {product.id}</div>
-                    <button onClick={onAddToBasketClicked}>Add to basket</button>
-                </>
+                <div className="SingleProduct__Container">
+                    <img className="SingleProduct__Image" src={product.img} alt={product.name} />
+                    <div className="SingleProduct__ProductDetails" >
+                        <h3 className="SingleProduct__Name">{product.name}</h3>
+                        <h4 className="SingleProduct__Colour">Colour: {product.colour}</h4>
+                        <div className="SingleProduct__Price">Price: {new Intl.NumberFormat("en-GB", { style: "currency", currency: "GBP" }).format(product.price)}</div>
+                        <div className="SingleProduct__ID">Product ID: {product.id}</div>
+                        <button onClick={onAddToBasketClicked}>Add to basket</button>
+                    </div>
+                </div>                  
             ) : (
                 <p>Product not found</p>
             )}
         </div>
-        
-        )
+    )
 }
 
 export default SingleProductPage;
